29dfbc0e5b362271a3121b7dd8adc9077daed953
Abigaell <alerryy1219@gmail.com>
2022-09-09 06:20:58 -0700

    2

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ * Description: prints alphabet in lowercase followed by a new line
+ *
+ * Return: Always 0
+ */
+int main(void)
+{
+	char letter;
+
+	for (letter = 'a'; letter <= 'z'; letter++)
+	putchar(letter);
+
+	putchar('\n');
+
+	return (0);
+
+}
+
