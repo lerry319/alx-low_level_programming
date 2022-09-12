a2c8e06d0e7d288db70f9c4293c2af11b723ae27
Abigaell <alerryy1219@gmail.com>
2022-09-12 02:21:28 -0700

    102

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -17,7 +17,7 @@ int main(void)
 			putchar((num1 % 10) + '0');
 			putchar(' ');
 			putchar((num2 / 10) + '0');
-			putcha((num2 % 10) + '0');
+			putchar((num2 % 10) + '0');
 
 			if (num1 == 98 && num2 == 9)
 				continue;
