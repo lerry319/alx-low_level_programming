92a13049ade56126b773d33a808a440c62a5640f
Abigaell <alerryy1219@gmail.com>
2022-09-13 02:36:58 -0700

    1

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/** 
+ * main - Prints _putchar
+ *
+ * Return: Always 0
+ */
+int main (void)
+{
+	_putchar("_putchar");
+	_putchar("\n");
+	return (0);
+}
---
+++ 0x02-functions_nested_loops/_putchar.c
@@ -0,0 +1,10 @@
+#include <unistd.h>
+/**
+ * _putchar - writes the character cto stdout
+ *
+ * Return: On success1.
+ */
+int _putchar(char c)
+{
+	return (write(1, &c, 1);
+}
