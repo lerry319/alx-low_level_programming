21e55e983b5be32eb5adfa5e7d1a9cf73fd49579
Abigaell <alerryy1219@gmail.com>
2022-09-15 11:47:18 -0700

    10

--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -8,5 +8,9 @@ int _abs(int n);
 int _isupper(int c);
 int _isdigit(int c);
 int mul(int a, int b);
+void print_numbers(void);
+void print_most_numbers(void);
+void more_numbers(void);
+void print_line(int n);
 
 #endif /* MAIN_H */
