96618fcf521228f22450bd11ffe297a8bb262841
Abigaell <alerryy1219@gmail.com>
2022-09-15 10:38:15 -0700

    2

--- 0x04-more_functions_nested_loops/2-mul.c
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -1,12 +1,14 @@
 #include "main.h"
 
 /**
- * _mul - multiplies int a and b
+ * mul - multiplies int a and b
  * @a: integer
  * @b: integer
  * Return: 0
  */
 int mul(int a, int b)
 {
-	return (a * b);
+	prod = a * b;
+	printf("%d\n", prod);
+	return (0);
 }
