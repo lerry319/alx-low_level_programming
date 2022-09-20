b9aecee9d412160983263b4f6bffcef6c0b25929
Abigaell <alerryy1219@gmail.com>
2022-09-20 11:40:28 -0700

    6

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,23 @@
+#include "main.h"
+/**
+ * puts2 - prints every character of a string strarting with 1st.
+ * @str: string
+ * Return: 0
+ */
+void puts2(char *str)
+{
+	int i, j;
+	j = 0;
+
+	while (str[j] != '\0')
+	{
+		j++;
+	}
+
+	for (i = 0; i < j; i += 2)
+	{
+		_putchar(str[i]);
+	}
+
+	_putchar('\n');
+}
