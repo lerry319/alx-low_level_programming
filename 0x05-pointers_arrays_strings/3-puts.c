c11d80944c3e173d298cc22b07bed1668ecdc495
Abigaell <alerryy1219@gmail.com>
2022-09-20 01:44:34 -0700

    3

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * _puts - prints a string followed by a new line
+ *@str: string
+ * Return: 0
+ */
+void _puts(char *str);
+{
+	char *str;
+	
+	str = " I do not fear computer.I fear the lack of them - Isaac Asimov";
+	_puts(str);
+
+	return (0);
+}
