868261620a95650b57708372a88fa6f8fae93cd4
Abigaell <alerryy1219@gmail.com>
2022-09-26 03:06:17 -0700

    9

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -11,26 +11,22 @@ void times_table(void)
 
 	for (x = 0; x <= 9; x++)
 	{
-		for (y = 0; y <= 9; y++)
+		_putchar('0');
+
+		for (y = 1; y <= 9; y++)
 		{
-			product = x * y;
-			if (y != 0)
-			{
-				_putchar(',');
-				_putchar(' ');
-			}
-			if (y == 0)
-			{
-				_putchar('0');
-			}
-			else if (product >= 10)
-			{
-				_putchar((product / 10) + '0');
-				_putchar((product % 10) + '0');
-			}
+			_putchar(',');
 			_putchar(' ');
+
+			product = x * y;
+
+			if (product <= 9)
+				_putchar(' ');
+			else
+				_putchar((product / 10) + '0');
+
 			_putchar((product % 10) + '0');
 		}
+		_putchar('\n');
 	}
-	_putchar('\n');
 }
