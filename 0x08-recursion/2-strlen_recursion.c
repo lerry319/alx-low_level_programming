39bb0b28715ca3c4d3dfffdc851f663c28c673e1
Abigaell <alerryy1219@gmail.com>
2022-09-29 04:30:25 -0700

    2

---
+++ 0x08-recursion/2-strlen_recursion.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ * _strlen_recursion - prints the lenth of a string
+ *
+ * @s: string
+ * Return: 0
+ */
+int _strlen_recursion(char *s)
+{
+	if (*s == '\0')
+		return (0);
+	return ( 1 + _strlen_recursion(s + 1));
+}	
